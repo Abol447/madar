33efa406fe076d856ef2aec376a6b68411413a45
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-05-06 06:52:28 +0330

    Add files via upload

---
+++ three/threeBehave.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:58:18 04/21/2025 
+// Design Name: 
+// Module Name:    threeBehave 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module threeBehave(
+    input [3:0] A,
+    input [3:0] B,
+    output G ,
+    output L ,
+    output E  
+	 );
+    assign E = (A == B);
+    assign G = (A > B) ; 
+    assign L = (A < B) ;
+
+endmodule
---
+++ three/threeTB.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:59:35 04/21/2025 
+// Design Name: 
+// Module Name:    threeTB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 1ps
+module threeTB(
+
+    );
+
+   reg [3:0] A;
+   reg [3:0] B;
+   wire G;
+   wire L;
+   wire E;
+
+threeBehave(
+            .A(A),
+            .B(B),
+            .G(G),
+            .L(L),
+            .E(E)
+);
+
+initial
+begin
+ 
+A = 4'd4;
+B = 4'd5;
+
+
+#50;
+
+A = 4'd6;
+B = 4'd8;
+
+end
+endmodule
