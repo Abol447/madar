0cb07a54e5c48380298696201fcda38174537c73
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-05-06 06:54:46 +0330

    Add files via upload

---
+++ two/twoBehave.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:40:36 04/21/2025 
+// Design Name: 
+// Module Name:    twoBehave 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module twoBehave(
+   input  [3:0] A,
+   input  [3:0] B,
+   input S,
+   input E,
+   output [3:0] Y 
+    );
+ assign Y = E ? (S? B:A): 4'b0000;
+
+endmodule
---
+++ two/twoTB.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:41:54 04/21/2025 
+// Design Name: 
+// Module Name:    twoTB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module twoTB(
+    );
+
+reg [3:0] A ;
+reg [3:0] B ; 
+reg E ; 
+reg S ;
+
+twobehave(
+          .A(A),
+          .B(B),
+          .E(E),
+          .S(S)
+);
+
+initial 
+ 
+begin
+
+A = 4'b0101;
+B = 4'b0110;
+E = 1'b1;
+S = 1'b1;
+
+#50;
+
+
+A = 4'b1000;
+B = 4'b0100;
+E = 1'b1;
+S = 1'b0;
+
+end 
+ 
+endmodule
