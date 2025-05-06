7db1f7e7fd8561a801773f374ecad0060f59ed76
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-05-06 06:51:16 +0330

    Add files via upload

---
+++ foure/foureBehave.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:02:12 04/21/2025 
+// Design Name: 
+// Module Name:    foureBehave 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module foureBehave(
+    
+  input [3:0] A,
+  input [3:0] B,
+  input C,
+  output [4:0] S,
+  output c4,
+  output v
+	 );
+assign S = A + C+ B;
+assign c4 = S[4];
+assign v = c4 ? 1'd1 : 1'd0;
+
+endmodule
---
+++ foure/foureTB.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:03:33 04/21/2025 
+// Design Name: 
+// Module Name:    foureTB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module foureTB(
+    );
+
+reg [3:0] A ; 
+reg [3:0] S;
+reg [3:0] B;
+reg C;
+reg C4;
+reg v;
+
+foureBehave(
+            .A(A),
+            .B(B),
+            .S(S),
+            .C(C),
+            .c4(C4),
+            .v(v)           
+);
+
+initial
+begin 
+
+A = 4'd4;
+B = 4'd5;
+C = 1'b1;
+
+#50
+
+A = 4'd12;
+B = 4'd9;
+C = 1'b1;
+
+end
+endmodule
